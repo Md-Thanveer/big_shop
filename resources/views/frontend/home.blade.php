f7c2e6800b536300a99f5512bc7098084c6bbd6b
Md-Thanveer <thanveerami612@gmail.com>
2024-08-16 12:20:19 +0530

    Homecontroller
    created the homecontroller and home blade template and its update the route for home app

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,10 @@
+@extends('frontend.layout.app')
+
+@section('title')
+home page
+
+@endsection
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+use App\Http\Controllers\HomeController;
+
+Route::get('/',[HomeController::class, 'index'])->name('home.index');
+
 use App\Http\Controllers\AuthController;
 
 Route::get('/forget_password',[AuthController::class, 'forget_password'])->name('home.forget_password');
+
+
+
