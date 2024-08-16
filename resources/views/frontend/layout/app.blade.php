1ba556a55cc3c52bae578c18998047bd6e82884b
Md-Thanveer <thanveerami612@gmail.com>
2024-08-16 18:46:46 +0530

    deal_of_the_day blade template
    in this slider folder deal_of_the_day folder updated the deal_of_the_Day code template

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -45,6 +45,9 @@
             <!-- Include the daily best sells slider partial -->
             @include('frontend.slider.daily_best_sells.type1')
 
+             <!-- Include the popular product slider partial -->
+             @include('frontend.slider.deal_of_the_day.type1')
+
     </main>
         @yield('content')
 
